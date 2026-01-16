e449d2a4018a1023bd1fcdd36f26b9e630c5dcf7
yhjiang504 <yhjiang504@gmail.com>
2026-01-16 15:32:46 +0800

    V2.7
    1.使用 IndexedDB
    2.變更新增票券後的排版
    3.新增Coupon來源欄位
    4.新增備份時間

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// 🔥 1. 這裡改成 v2.3.1，強迫手機更新
-const CACHE_NAME = 'flower-money-v2.3.1';
+// 🔥 1. 這裡改成 v2.7，強迫手機更新
+const CACHE_NAME = 'flower-money-v2.7';
 
 // 安裝階段 (Install)
 self.addEventListener('install', (e) => {
