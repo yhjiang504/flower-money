2be802a71790fcffdd4d5a6e525cc212ee549634
yhjiang504 <yhjiang504@gmail.com>
2026-05-29 13:28:52 +0800

    V3.1

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-// 🌸 花花大金庫 V3.0 Service Worker
+// 🌸 花花大金庫 V3.1 Service Worker
 // 版本更新時，改這裡的版本號即可強制手機更新快取
-const CACHE_NAME = 'flower-money-v3.0';
+const CACHE_NAME = 'flower-money-v3.1';
 
 // 安裝階段：快取核心資源
 self.addEventListener('install', e => {
